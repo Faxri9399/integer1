6833236c3d23d57f4397fbf0f0590541a1506124
TEXNO <fbbozorov@gmail.com>
2023-12-03 15:01:24 +0500

    integer1

---
+++ script.js
@@ -0,0 +1,44 @@
+//integer-1
+/*let c;
+let a=8;
+let b=5;
+c = Math.sqrt(Math.sqrt((a**3-b**3)+(a**3+b**3)));
+console.log(c);*/
+//integer-2
+/*let c;
+let a=4;
+let b=9;
+c=(1/6*Math.sqrt(a)+1/3*Math.sqrt(b))**2;
+console.log(c);*/
+//integer-3
+/*let n;
+n=521;
+console.log(n.toString().split("").reverse().join());*/
+//integer-4
+/*let a = 345;
+
+console.log(a);*/
+//integer-5
+/*let a = 6.3;
+let b = 8.6;
+c = 9*a**2*b-27*a**2*b**2+15*b**2;
+console.log(c.toFixed(2));*/
+//integer-6
+/*let r = 3;
+n = 2; a = 4; b = 5;
+k = Math.pow(1+r/100,n)/Math.sqrt(a**2+b**2);
+console.log(k);*/
+//integer-7
+/*let x1 = -1.4;
+y1 = 7.9; x2 = 6.1; y2 = 9.9;
+d = Math.sqrt((x2-x1)**2+(y2-y1)**2);
+console.log(d);*/
+//integer-9
+/*let a = -0.5;
+x = Math.sin(a)
+console.log(x);*/
+//integer-10
+/*let a = 0.5;
+x = Math.cos(a);
+console.log(x);*/
+
